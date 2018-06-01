706a8b459383b1ff58ba438ab5e63d9efc0bcd3c
Mikhail Mekhedkin Meskhi <michael.m.meskhi@live.com>
2018-06-01 18:14:03 +0400

    Ready for release

--- bsda.py
+++ bsda.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Support Vector Machine using active learning with UncertaintySampling
+Multi-Layer Neural Network and Support Vector Machine
+Using active learning with UncertaintySampling
 Author: Michael M.Meskhi
 Project: Broadscale Domain Adaptation with Active Learning
-Date: 2018-05 -28
+Date: 2018-06-01
 """
 
 import os
@@ -12,6 +13,7 @@ import sys
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
 try:
     from sklearn.model_selection import train_test_split
 except ImportError:
@@ -24,8 +26,12 @@ from libact.query_strategies import *
 from libact.labelers import IdealLabeler
 
 
-def results(accuracy):
-    print("Standard Deviation: " + str(np.std(accuracy)) + "\n" + "Mean Accuracy: " + str(np.mean(accuracy)))
+def results(accuracy, model_name):
+    print(model_name)
+    print("==========================")
+    print("Mean Accuracy: " + str(round(np.mean(accuracy)*100, 2)) + "\n" + "Standard Deviation: " + str(round(np.std(accuracy), 2)))
+    print("==========================\n")
+
 
 def run(trn_ds, tst_ds, lbr, model, qs, quota):
     E_in, E_out, accuracy = [], [], []
@@ -63,10 +69,9 @@ def main():
 
     # Path to your libsvm_sparse type classification dataset.
     # If dataset not in libsvm_sparse type use libsvm to convert.
-    visuals_path = '../vis'
     dataset_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), sys.argv[1])
-    test_size = float(sys.argv[2])    # The percentage of samples in the dataset that will be randomly selected and assigned to the test set.
-    n_labeled = int(sys.argv[3])     # Number of samples that are initially labeled.
+    test_size = float(sys.argv[2])   # The percentage of samples in the dataset that will be randomly selected and assigned to the test set.
+    n_labeled = int(sys.argv[3])   # Number of samples that are initially labeled.
 
     # Load dataset
     trn_ds, tst_ds, y_train, fully_labeled_trn_ds = split_train_test(dataset_filepath, test_size, n_labeled)
@@ -76,32 +81,36 @@ def main():
     quota = int(sys.argv[4])   # Number of samples to query.
 
     # Model is the base learner, e.g. LogisticRegression, SVM ... etc.
-    qs = UncertaintySampling(trn_ds, method='lc', model=SVM(gamma=0.0001))
-    model = SVM()
-    E_in_1, E_out_1, accuracy = run(trn_ds, tst_ds, lbr, model, qs, quota)
+    models = {'MLP': SklearnProbaAdapter(MLPClassifier(hidden_layer_sizes=(10,10,10,10,10,10), solver='lbfgs', alpha=2, random_state=1, activation='relu')), 
+            'SVM': SVM(gamma=0.001)}
+    
+    for model_name, model in models.items():
+        qs = UncertaintySampling(trn_ds, method='lc', model=model)
+        E_in_1, E_out_1, accuracy = run(trn_ds, tst_ds, lbr, model, qs, quota)
 
-    # Plot the learning curve of UncertaintySampling.
-    # The x-axis is the number of queries, and the y-axis is the corresponding error rate.
-    query_num = np.arange(1, quota + 1)
-    plt.plot(query_num, E_in_1, 'b', label='qs Ein')
-    plt.plot(query_num, E_out_1, 'g', label='qs Eout')
-    plt.xlabel('Number of Queries')
-    plt.ylabel('Error')
-    plt.title('Experiment Result')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
-    plt.show()
+        # Plot the learning curve of UncertaintySampling.
+        # The x-axis is the number of queries, and the y-axis is the corresponding error rate.
+        query_num = np.arange(1, quota + 1)
+        plt.plot(query_num, E_in_1, 'b', label='qs Ein')
+        plt.plot(query_num, E_out_1, 'g', label='qs Eout')
+        plt.xlabel('Number of Queries')
+        plt.ylabel('Error')
+        plt.title('Experiment Result')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
+        plt.show()
 
-    # Plot the accuracy over requested queries. 
-    # The x-axis is the number of queries, and the y-axis is the corresponding accuracy rate.
-    plt.plot(query_num, accuracy, 'y', label="accuracy")
-    plt.xlabel('Number of Queries')
-    plt.ylabel('Accuracy')
-    plt.title('SVM + Active Learning')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.8, -0.5), fancybox=True, shadow=True, ncol=5)
-    plt.savefig('vis/svm.png')
-    plt.show()
+        # Plot the accuracy over requested queries. 
+        # The x-axis is the number of queries, and the y-axis is the corresponding accuracy rate.
+        filename = 'results/' + model_name + '.png'
+        plt.plot(query_num, accuracy, 'y', label="accuracy")
+        plt.xlabel('Number of Queries')
+        plt.ylabel('Accuracy')
+        plt.title(model_name + ' + Active Learning')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.8, -0.5), fancybox=True, shadow=True, ncol=5)
+        plt.savefig(filename)
+        plt.show()
 
-    results(accuracy)
+        results(accuracy, model_name)
 
 if __name__ == '__main__':
     main()
